57913dc433a4c10177c49b4a4cff171b80c5a176
Tomás Andres <95324804+ThomKhas@users.noreply.github.com>
2023-11-08 01:40:53 -0300

    maquetacion del estilo de page de emp aplicado

--- app/pages/emp/page.jsx
+++ app/pages/emp/page.jsx
@@ -1,9 +1,28 @@
-"use client";
+'use client';
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import 'bootstrap/dist/css/bootstrap.css';
 import Image from 'next/image';
+
+function Navbar({ logout }) {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <img
+          className="img-fluid"
+          src="/src/minilogo.png"
+          alt="logo para btn"
+          width={190}
+          height={130}
+        />
+        <button className="btn btn-primary" type="submit" onClick={logout}>
+          Salir
+        </button>
+      </div>
+    </nav>
+  );
+}
 
 function EmpPage() {
   const [user, setUser] = useState({
@@ -38,31 +57,38 @@ function EmpPage() {
   };
 
   return (
-      <div>
-      <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-        <Image className="img-fluid" 
-        src="/src/minilogo.png"
-        alt="logo para btn"
-        width={190}
-        height={130}/>
-            <button className="btn btn-primary" type="submit" onClick={() => logout()}>Salir</button>
+    <div style={{ backgroundColor: '#DBD7D7', minHeight: '100vh' }}>
+      <Navbar logout={logout} />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-6" style={{ backgroundColor: '#DBD7D7', padding: '50px' }}>
+            <div style={{ textAlign: 'left', marginLeft: '250px' }}>
+              <h1 style={{ textAlign: 'left', marginTop: '200px' }}>PERFIL DE EMPLEADO</h1>
+              <h4 style={{ textAlign: 'left' }}>
+                Bienvenido, {user.full_name}
+                <br /> Por favor seleccione una de las opciones para continuar.
+              </h4>
+              <h2 style={{ textAlign: 'left', marginTop: '70px'}}>Opciones</h2>
+              <div style={{ marginTop: '10px' }}>
+                <button className="btn btn-success" onClick={goToViewPage}>
+                  Ver Perfil
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6" style={{ backgroundColor: '#DBD7D7', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ marginTop: '120px', marginRight: '190px' }}>
+              <Image
+                src="/src/logoPage.png"
+                alt="Ejemplo de imagen"
+                width={940}
+                height={505}
+              />
+            </div>
+          </div>
         </div>
-      </nav>
-      <div style={{ height: '94vh', backgroundColor: '#DBD7D7'}}>
-
-      <h1 style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left',marginLeft: '50px', color: 'black' }}>PERFIL DE EMPLEADO</h1>
-
-      <h4 style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left',marginLeft: '50px', color: 'black', textAlign: 'left', }}>Bienvenido, {user.full_name}<br/> Porfavor seleccione una de las opciones para continuar.</h4>
-
-      <h2 style={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', marginLeft: '50px', color: 'black' }}>Opciones</h2>
-
-      <div style={{ display: 'flex', justifyContent: 'left', marginTop: '10px', marginLeft: '50px' }}>
-        <button className="btn btn-success" onClick={goToViewPage}>Ver usuarios</button>
       </div>
-
-      </div>
-      </div>
+    </div>
   );
 }
 
